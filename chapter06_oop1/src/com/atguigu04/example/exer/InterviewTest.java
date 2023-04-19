c036b9f6f083a927a484d8008a744eaebf22e3e2
allen_yale <137728688@qq.com>
2023-04-19 18:28:38 +0800

    面试题，方法重载，println有char[]为参数，直接打印出字符

---
+++ chapter06_oop1/src/com/atguigu04/example/exer/InterviewTest.java
@@ -0,0 +1,29 @@
+package com.atguigu04.example.exer;
+
+public class InterviewTest {
+    public static void main(String[] args) {
+        char[] crr = new char[]{'a','b','c','a'};
+        int[] intArr = new int[]{1,3,2,5};
+        boolean[] brr = new boolean[]{true,false,true};
+
+        /*
+        abca
+        [I@4554617c
+        [Z@74a14482
+         */
+        /*
+        public void println(char x[]) {
+        synchronized (this) {
+            print(x);
+            newLine();
+        }
+    }
+    public void print(char s[]) {
+        write(s);
+    }
+         */
+        System.out.println(crr);
+        System.out.println(intArr);
+        System.out.println(brr);
+    }
+}
